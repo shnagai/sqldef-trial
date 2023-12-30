3b6e3b60994b5b7d092c8e34f3a114ee5ae35bf0
shnagai <sho1ro1101@gmail.com>
2023-12-30 17:32:22 +0900

    add users.age

--- schema.sql
+++ schema.sql
@@ -1,6 +1,7 @@
 CREATE TABLE "public"."users" (
     "id" serial NOT NULL,
     "name" character varying(255) NOT NULL,
+    "age" integer NOT NULL,
     "created" timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
     PRIMARY KEY ("id")
 );
